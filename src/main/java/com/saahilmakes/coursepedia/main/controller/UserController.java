41384295bcef72d137ce82a710ffcd9bbd6510c4
saahilanande <saahilanande21@gmail.com>
2023-05-10 15:58:08 -0500

    Update user Api Added

--- src/main/java/com/saahilmakes/coursepedia/main/controller/UserController.java
+++ src/main/java/com/saahilmakes/coursepedia/main/controller/UserController.java
@@ -4,7 +4,6 @@ package com.saahilmakes.coursepedia.main.controller;
 import com.saahilmakes.coursepedia.main.model.UserModel;
 import com.saahilmakes.coursepedia.main.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -59,19 +58,23 @@ public class UserController {
         }
     }
 
-    //Endpoint to update a particular user
+    //Endpoint to update a particular user with Id
     @PutMapping("/updateuser/{userId}")
     public String updateUser(@PathVariable("userId") String userId, @RequestBody UserModel user) {
         try{
             UserModel updateuser = new UserModel();
             updateuser.setId(userId);
             updateuser.setName(user.getName());
-            userInterface.save(updateuser);
+            updateuser.setPhone(user.getPhone());
+            updateuser.setAge(user.getAge());
+            updateuser.setEmail(user.getEmail());
+            updateuser.setPassword(user.getPassword());
+            UserModel updatedUser = userInterface.save(updateuser);
+            return "Updated Succesfully";
         }
-        catch (Exception ex){
-
+        catch (Exception ex) {
+            return "Error: "+ex;
         }
-        return "User updated";
     }
 
     //Endpoint to Delete a particular user
