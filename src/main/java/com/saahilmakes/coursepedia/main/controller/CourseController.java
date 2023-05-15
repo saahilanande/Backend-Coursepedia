6436b15c7131fb339169b57192c7344143fbaa21
saahilanande <saahilanande21@gmail.com>
2023-05-15 12:34:59 -0500

    Created a Endpoint to Add a new course

--- src/main/java/com/saahilmakes/coursepedia/main/controller/CourseController.java
+++ src/main/java/com/saahilmakes/coursepedia/main/controller/CourseController.java
@@ -1,7 +1,10 @@
 package com.saahilmakes.coursepedia.main.controller;
 
+import com.saahilmakes.coursepedia.main.model.CourseModel;
 import com.saahilmakes.coursepedia.main.repository.CourseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,22 @@ public class CourseController {
 
     @Autowired
     CourseRepo courseInterface;
+
+    //Endpoint to add a new course
+    @PostMapping("/addcourse")
+    public CourseModel addUserById(@RequestBody CourseModel reqcourse) {
+
+        CourseModel newCourse = new CourseModel();
+        newCourse.setName(reqcourse.getName());
+        newCourse.setDescription(reqcourse.getDescription());
+        newCourse.setDifficulty(reqcourse.getDifficulty());
+        newCourse.setLink(reqcourse.getLink());
+        newCourse.setType(reqcourse.getType());
+        newCourse.setCategory(reqcourse.getCategory());
+        newCourse.setVideo(reqcourse.getVideo());
+
+        courseInterface.insert(newCourse);
+
+        return newCourse;
+    }
 }
