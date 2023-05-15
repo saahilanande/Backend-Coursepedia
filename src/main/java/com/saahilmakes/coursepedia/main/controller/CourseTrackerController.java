29c07cac229ac70b7709a3444c3fbbfda70be621
saahilanande <saahilanande21@gmail.com>
2023-05-15 16:42:54 -0500

    Add course to Tracker and Get all track course Endpoints Created

--- src/main/java/com/saahilmakes/coursepedia/main/controller/CourseTrackerController.java
+++ src/main/java/com/saahilmakes/coursepedia/main/controller/CourseTrackerController.java
@@ -1,4 +1,39 @@
 package com.saahilmakes.coursepedia.main.controller;
 
+import com.saahilmakes.coursepedia.main.model.CourseTrackerModel;
+import com.saahilmakes.coursepedia.main.repository.CourseTrackerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tracker")
 public class CourseTrackerController {
+
+    @Autowired
+    CourseTrackerRepo trackerInterface;
+
+    //Get All tracked Courses
+    @GetMapping("")
+    public List<CourseTrackerModel> getAllTrackedCourse(){
+       List<CourseTrackerModel> trackedCourses = trackerInterface.findAll();
+       return trackedCourses;
+    }
+
+    //Add a course to track
+    @PostMapping("/Addtracker/{course_id}/{user_id}")
+    public String addTracker(@PathVariable("course_id") String course_id, @PathVariable("user_id") String user_id){
+        try {
+            CourseTrackerModel newTracker = new CourseTrackerModel();
+            newTracker.setCourse_id(course_id);
+            newTracker.setUser_id(user_id);
+            trackerInterface.insert(newTracker);
+            return "Course Added to Track Successfully";
+        }
+        catch (Exception ex){
+            return ""+ex;
+        }
+    }
+
 }
