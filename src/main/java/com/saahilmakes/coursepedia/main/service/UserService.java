8dd05d704b68bc8da6b1e1ecd930be05cd312528
saahilanande <saahilanande21@gmail.com>
2023-05-28 13:04:47 -0500

    User service added and overide the UserDetailsService

--- src/main/java/com/saahilmakes/coursepedia/main/service/UserService.java
+++ src/main/java/com/saahilmakes/coursepedia/main/service/UserService.java
@@ -1,5 +1,9 @@
 package com.saahilmakes.coursepedia.main.service;
 
+import com.saahilmakes.coursepedia.main.model.UserModel;
+import com.saahilmakes.coursepedia.main.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepo userRepo;
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        UserModel userFound = userRepo.findUsername(s);
+        if(userFound == null){
+            return null;
+        }
+        return new User(userFound.getName(),userFound.getPassword(),null);
     }
 }
